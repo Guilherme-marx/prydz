6709a18e4c421b87d9214b383baae2d26bab5ccf
Guilherme Marques <gugosin3@gmail.com>
2022-12-19 16:11:23 -0300

    javascript

---
+++ html/valida.js
@@ -0,0 +1,39 @@
+// function validateEmail(email) {
+//     var re = /\S+@\S+\.\S+/;
+//     return re.test(email);
+//   }
+      
+//   console.log(validateEmail('texto@texto.com')); // true
+//   console.log(validateEmail('texto@texto')); // false
+//   console.log(validateEmail('texto.com')); // false
+//   console.log(validateEmail('texto')); // false
+
+function validacaoEmail(field) {
+usuario = field.value.substring(0, field.value.indexOf("@"));
+dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+
+if ((usuario.length >=1) &&
+    (dominio.length >=3) &&
+    (usuario.search("@")==-1) &&
+    (dominio.search("@")==-1) &&
+    (usuario.search(" ")==-1) &&
+    (dominio.search(" ")==-1) &&
+    (dominio.search(".")!=-1) &&
+    (dominio.indexOf(".") >=1)&&
+    (dominio.lastIndexOf(".") < dominio.length - 1)) {
+document.getElementById("msgemail").innerHTML="E-mail válido";
+alert("E-mail valido");
+}
+else{
+document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
+alert("E-mail invalido");
+}
+}
+
+function IsEmail(email){
+    var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
+    var check=/@[w-]+./;
+    var checkend=/.[a-zA-Z]{2,3}$/;
+    if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
+    else {return true;}
+}
