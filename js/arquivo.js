3b5604d82ec158c3d1ed8e7616d67215c2c4a759
Guilherme Marques <gugosin3@gmail.com>
2022-12-27 11:52:38 -0300

    validacao funcional

--- js/arquivo.js
+++ js/arquivo.js
@@ -4,7 +4,7 @@ let form = document.querySelector('form');
 let textForm = document.getElementById('texForm')
 // let textEmail = document.getElementById('texEmail')
 // let textPassword = document.getElementById('textPassword')
-let ReEmail = document.getElementById('ReEmail')
+// let ReEmail = document.getElementById('ReEmail')
 
 form.addEventListener('submit' , (e) => {
     if(email.value == '' && password.value == '') {
@@ -22,7 +22,7 @@ form.addEventListener('submit' , (e) => {
     e.preventDefault()
 })
 
-email.addEventListener('keyip' , ( ) => {
+email.addEventListener('keyup' , ( ) => {
     if(validatorEmail(email.value) !== true) {
         textEmail.textContent = 'insira um email valido -- Ex: abc@emial.com --'
     }
@@ -44,7 +44,32 @@ function validatorPassword(password) {
     let passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/;
     return passwordPattern.test(password)
 }
-function validatorReEmail(ReEmail) {
-    let ReEmailpattern =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/ 
-    return ReEmailPattern.test(ReEmail)
-}
+// function validatorReEmail(ReEmail) {
+//     let ReEmailpattern =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/ 
+//     return ReEmailPattern.test(ReEmail)
+// }
+function validarSenha(nome1, nome2) {
+    var password = document.getElementById(nome1).value;
+    var checkPassword = document.getElementById(nome2).value;
+    console.log(checkPassword);
+    if (password != "" &&
+      checkPassword != "" &&
+      password === checkPassword) {
+      alert('senha iguais');
+    } else {
+      alert('senhas diferentes');
+    }
+  }
+
+ function validarEmail(nome1, nome2) {
+    var email1 = document.getElementById(nome1).value;
+    var email2 = document.getElementById(nome2).value;
+    console.log(email2);
+    if (email1 != "" &&
+      email2 != "" &&
+      email1 === email2) {
+      alert('senha iguais');
+    } else {
+      alert('senhas diferentes');
+    }
+  }
