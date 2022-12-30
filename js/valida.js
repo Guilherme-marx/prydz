ed56faaf27a343dc41488935572282ca39bfb5de
Guilherme Marques <gugosin3@gmail.com>
2022-12-30 12:05:06 -0300

    testes

--- js/valida.js
+++ js/valida.js
@@ -10,81 +10,8 @@ function validarEmail(nome3, nome4) {
       alert('não são o mesmo email');
     }
   }
-
-/  PARA AVALIAR EM CASA QUAL FAZ MAIS SENTIDO PARA A VALIDAÃO DO FORMULARIO!!!! /
-
-
-//   email.addEventListener('keyup' , ( ) => {
-//       if(validarEmail(email.value) !== true) {
-//           textEmail.textContent = 'insira um email valido -- Ex: abc@emial.com --'
-//         }
-// })
-
-// function validarEmail(email) {
-//     let emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-    
-//     return emailPattern.test(email)
-// }
-
-// function validarEmail(field) {
-//     usuario = field.value.substring(0, field.value.indexOf("@"));
-//     dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
-    
-//     if ((usuario.length >=1) &&
-//     (dominio.length >=3) &&
-//     (usuario.search("@")==-1) &&
-//         (dominio.search("@")==-1) &&
-//         (usuario.search(" ")==-1) &&
-//         (dominio.search(" ")==-1) &&
-//         (dominio.search(".")!=-1) &&
-//         (dominio.indexOf(".") >=1)&&
-//         (dominio.lastIndexOf(".") < dominio.length - 1)) {
-//     document.getElementById("email").innerHTML="E-mail válido";
-//     alert("email valido");
-// }
-// else{
-//     document.getElementById("email").innerHTML="<font color = 'red'>E-mail inválido </font>";
-//     alert("E-mail inválido");
-// }
-// }
-
-// form.addEventListener('submit' , (e) => {
-//   if(email.value == '' && password.value == '') {
-//       textForm.textContent = 'você precisa preencher todos os campos'
-//   } else if(validarEmail(email.value) === true && validarPassword(passworde.value) === true) {
-//       textForm.textContent = '';
-//       textEmail.textContent = '';
-//       textPassword.textContent = '';
-//   }
+  function getRandomInt(max) {
+    return Math.floor(Math.random(1001) * 8999);
+  }
   
-//   else {
-//       console.log('preencha todos os campos');
-//   }
-
-//   e.preventDefault()
-// })
-
-
-//--------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//--------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------
+  console.log(getRandomInt()+1000);
