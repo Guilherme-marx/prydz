ea2161bcc731992d5cb1ff28b0de56a84d0ac07d
Guilherme Marques <gugosin3@gmail.com>
2023-01-04 14:10:17 -0300

    validação ok

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,22 +1,22 @@
 function validarSenha(nome1, nome2) {
-    var password = document.getElementById(nome1).value;
-    var checkPassword = document.getElementById(nome2).value;
-    console.log(checkPassword);
-    if (password != "" &&
-      checkPassword != "" &&
-      password === checkPassword) {
-      console.log('senha iguais');
-    } else {
-      console.log('senhas diferentes');
-    }
+  var password = document.getElementById(nome1).value;
+  var checkPassword = document.getElementById(nome2).value;
+  console.log(checkPassword);
+  if (password !== "" &&
+    checkPassword !== "" &&
+    password === checkPassword) {
+    alert('senha iguais');
+  } else {
+    alert('senhas diferentes');
   }
+}
 
-  (function() {
-    const random = (min, max) => Math.floor(Math.random() * (min + max) + min);
-    log(random(1001, 9998));
-  })();
+(function () {
+  const random = (min, max) => Math.floor(Math.random() * (min + max) + min);
+  log(random(1001, 9998));
+})();
 
-  function log(msg) {
-    const logElem = document.querySelector(".log");
-    logElem.innerHTML += `${msg}<br>`;
-  }
+function log(msg) {
+  const logElem = document.querySelector(".log");
+  logElem.innerHTML += `${msg}<br>`;
+}
